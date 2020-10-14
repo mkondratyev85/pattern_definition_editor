85fb39a0ea3328716ca4407c1eb3d34700bd69e8
Mikhail Kondratyev <mkondratyev85@gmail.com>
2020-10-14 12:04:42 +1100

    I can drag base point

--- view.py
+++ view.py
@@ -5,8 +5,12 @@ import math
 
 from sidepanel import SidePanel
 
+DEFAULT = 0
+MOVE_BASE_POINT = 1
+
 class View:
     def __init__(self, root, model):
+        self.mode = DEFAULT
         self.model = model
         self.frame = tk.Frame(root)
         self.frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
@@ -19,14 +23,37 @@ class View:
 
         self.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
+        self.canvas.bind('<ButtonPress-1>', self.on_line_click)
+        self.canvas.bind('<ButtonRelease-1>', self.on_line_release)
+        self.canvas.bind('<B1-Motion>', self.on_motion)
+
+        self.selected_line_id = None
+
     def clear(self, event):
         self.canvas.delete('all')
+
+    def on_line_release(self, event):
+        self.mode = DEFAULT
+
+    def on_motion(self, event):
+        if self.mode == DEFAULT:
+            return
+        x, y = event.x, event.y
+        line = self.get_line_by_object_id(self.selected_line_id)
+        line.base_point = x, y
+        self.draw_anchors(self.selected_line_id)
+        print('motion')
 
     def on_line_click(self, event):
         clicked_object_id = event.widget.find_closest(event.x, event.y)[0]
         line = self.get_line_by_object_id(clicked_object_id)
-        self.draw_anchors(clicked_object_id)
-        print(line)
+        if line:
+            self.draw_anchors(clicked_object_id)
+            print(line)
+        if clicked_object_id == self.base_point_anchor_id:
+            print('ya')
+            self.mode = MOVE_BASE_POINT
+        
 
     def draw_lines(self, event):
         self.canvas.delete('all')
@@ -34,16 +61,12 @@ class View:
             x0, y0 = line.base_point
             x1, y1 = line.second_point
 
-            # draw line
             line_id = self.canvas.create_line(x0, y0, x1, y1)
             line.canvas_line_id = line_id
-            # print(type(line))
 
-            self.canvas.bind('<ButtonPress-1>', self.on_line_click)
-
-            # draw base_point
-            # self.canvas.create_rectangle(x0-RW, y0-RW, x0+RW, y0+RW)
-        self.draw_anchors(self.model.lines[0].canvas_line_id, new=True)
+        first_line_id = self.model.lines[0].canvas_line_id
+        self.selected_line_id = first_line_id
+        self.draw_anchors(first_line_id, new=True)
 
     def get_line_by_object_id(self, canvas_object_id):
         line = next(filter(lambda x: x.canvas_line_id == canvas_object_id, self.model.lines), None)
